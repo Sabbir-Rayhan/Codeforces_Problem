f205b9db67491f7c256613ef08ef1c0334836063
Sabbir-Rayhan <126398831+Sabbir-Rayhan@users.noreply.github.com>
2023-08-16 11:18:25 +0600

    Create Buttons.cpp

---
+++ Buttons.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+	long long a,b,c;
+	cin>>a>>b>>c;
+	if(a==b){
+		if(c%2==0)
+			cout<<"Second"<<endl;
+		else
+			cout<<"First"<<endl;
+	}
+
+	else if(a==b && b==c){
+		if(a%2==0)
+			cout<<"Second"<<endl;
+		else
+			cout<<"First"<<endl;
+	}
+
+	else{
+		if(a>b)
+			cout<<"First"<<endl;
+		else
+			cout<<"Second"<<endl;
+	}
+}
+}
